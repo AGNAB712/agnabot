d2b6b9f9c92fdc996ce795b17b0397016df41252
AGNAB712 <agnab712@gmail.com>
2023-09-24 11:18:44 -0500

    general touchups

---
+++ info/buyMap.js
@@ -0,0 +1,20 @@
+const buyArray = 
+[
+{ label: 'Cancel', description: 'deletes this menu', value: 'cancel', emoji: '1153460434691698719' }, 
+{ label: 'HOTEL', description: '5,000 AGNABUCKS \n(buys a hotel, use a.hotel buy!)', value: 'hotel', emoji: '🏨' }, 
+{ label: 'Cocaina', description: '10,000 AGNABUCKS \n(high for one day)', value: 'cocaine', emoji: '1154570594797486100' }, 
+{ label: 'Methamphetamin', description: '1,000 AGNABUCKS \n(high for one hour)', value: 'meth', emoji: '🍃' },
+{ label: 'Alcohol', description: '100 AGNABUCKS \n(high for one minute)', value: 'alcohol', emoji: '🍺' }, 
+{ label: 'AGNAB premium', description: '10,000 AGNABUCKS \n(access to secret chat + 25% boost to a.work)', value: 'premium', emoji: '1120846837436399718' }, 
+{ label: 'Thermonuclear bomb', description: '1,000 AGNABUCKS \n(mention someone and they get muted for a minute)', value: 'bomb', emoji: '💣' }, 
+{ label: 'Rigged slot machine', description: '5,000 AGNABUCKS \n(increases the chance of you winning in double or nothing to 60%)', value: 'rigged', emoji: '🎰' },
+{ label: 'TTS pass', description: '100 AGNABUCKS \n(one time a.tts usage)', value: 'tts', emoji: '🎟️' }, 
+{ label: 'Speechbubble', description: '200 AGNABUCKS \n(buys a random speechbubble from the speechbubble pool', value: 'speechbubble', emoji: '🗨️' }, 
+{ label: 'Child labor', description: '[money] AGNABUCKS \n(one agnabuck every 5 minuto per level)', value: 'child', emoji: '🚸' }, 
+{ label: 'Wedding ring', description: '10,000 AGNABUCKS \n(you can propose to someone Yipee)', value: 'ring', emoji: '💍' },
+{ label: 'Fishing Rod', description: '5,000 AGNABUCKS (unlocks fishing)', value: 'fish', emoji: '🎣' },
+]
+
+module.exports ={
+	buyArray
+}
