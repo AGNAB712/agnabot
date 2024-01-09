908a74cd98ff3f063cb54500da79b00ab4eace43
AGNAB <agnab712@gmail.com>
2024-01-09 07:16:04 -0600

    whoops
    yeah i make a lot of these kind of commits

--- commands/fun/badtranslate.js
+++ commands/fun/badtranslate.js
@@ -6,14 +6,13 @@ const { getGlobalVar, setGlobalVar } = require('../../info/editGlobalJson.js')
 const { hasArtifact, getRandomInt, isNumeric, validUserId } = require('../../info/generalFunctions.js')
 const { workArray } = require('../../info/agnabot_work_texts.js')
 const db = new QuickDB();
-const { translate } = require('@vitalets/google-translate-api');
 const translate2 = require('translate-google');
 const languages = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'ny', 'zh-CN', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'iw', 'hi', 'hmn', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'rw', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'or', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
 
 async function badtranslate(message, args, bot, client) {
   let translatedText = args.join(' ');
 
-  const repliedMessage = await message.channel.messages.fetch(message.reference.messageId)
+  const repliedMessage = await message.channel.messages.fetch(message.reference?.messageId)
   if (repliedMessage.content) {translatedText = repliedMessage.content}
   if (!translatedText) {return message.reply('**<:AgnabotX:1153460434691698719> ||** that message does NOT have text!')}
 
