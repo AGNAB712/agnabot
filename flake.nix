d9035f6a99236fed7accbc09a67740bde6a73cd9
AGNAB <133778639+AGNAB712@users.noreply.github.com>
2026-02-01 02:05:49 -0600

    ok hopefully this works

--- flake.nix
+++ flake.nix
@@ -9,12 +9,27 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      # import the node2nix-generated packages
       nodePackages = import ./node-packages.nix { inherit pkgs; };
     in {
+      # deployable package
       packages.${system} = {
         agnabot = nodePackages.agnabot;
       };
-      # this exposes it directly as self.packages.${system}.agnabot
-      defaultPackage.${system} = nodePackages.agnabot;
+
+      # defaultPackage points to the deployable package
+      defaultPackage.${system} = self.packages.${system}.agnabot;
+
+      # development shell
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs
+          nodePackages.agnabot
+        ];
+
+        shellHook = ''
+          export NODE_PATH=${nodePackages.agnabot}/lib/node_modules
+        '';
+      };
     };
 }
