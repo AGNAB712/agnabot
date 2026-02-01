d2541a76715babf51e496328cb8cafbcd2deebb6
AGNAB <133778639+AGNAB712@users.noreply.github.com>
2026-02-01 10:29:11 -0600

    ugh

--- index.js
+++ index.js
@@ -2,16 +2,8 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits, Partials, ActivityType, ActionRowBuilder, ButtonBuilder, ButtonStyle, WebHookClient, AttachmentBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, PermissionsBitField, EmbedBuilder } = require("discord.js");
 const fs = require('fs');
-const { QuickDB } = require("quick.db");
-const dataDir =
-  process.env.AGNABOT_DATA_DIR ??
-  path.join(os.homedir(), ".local/share/agnabot")
+const path = require('path');
 
-fs.mkdirSync(dataDir, { recursive: true })
-
-const db = new QuickDB({
-  filePath: path.join(dataDir, "json.sqlite")
-})
 //require misc functions
 const { autoReact } = require('./info/autoReactions.js')
 const { getGlobalVar, setGlobalVar } = require('./info/editGlobalJson.js')
@@ -22,6 +14,18 @@ const { marriageImage, podium, fetchProfilePicture, balance, petImage } = requir
 const { google } = require('googleapis');
 const credentials = require('./info/googleServiceAccount.json')
 const os = require('os')
+
+//quickdb
+const { QuickDB } = require("quick.db");
+const dataDir =
+  process.env.AGNABOT_DATA_DIR ??
+  path.join(os.homedir(), ".local/share/agnabot")
+
+fs.mkdirSync(dataDir, { recursive: true })
+
+const db = new QuickDB({
+  filePath: path.join(dataDir, "json.sqlite")
+})
 
 //import commands
 const commands = {}
