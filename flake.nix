36d244ff3899a4b50b508af736caffb0992b3494
AGNAB <133778639+AGNAB712@users.noreply.github.com>
2026-02-01 09:47:50 -0600

    ok trying the flake from the wiki

--- flake.nix
+++ flake.nix
@@ -1,38 +1,19 @@
 {
-  description = "agnabot flake";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      # import the node2nix-generated packages
-      nodePackages = import ./node-packages.nix {
-        inherit (pkgs) nodeEnv fetchurl fetchgit stdenv lib;
-        nix-gitignore = pkgs.nix-gitignore;
-        };
-    in {
-      # deployable package
-      packages.${system} = {
-        agnabot = nodePackages.agnabot;
-      };
-
-      # defaultPackage points to the deployable package
-      defaultPackage.${system} = self.packages.${system}.agnabot;
-
-      # development shell
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs
-          nodePackages.agnabot
-        ];
-
-        shellHook = ''
-          export NODE_PATH=${nodePackages.agnabot}/lib/node_modules
-        '';
-      };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    packages."x86_64-linux".default = pkgs.buildNpmPackage {
+      pname = "agnabot";
+      version = "1.0.0";
+      src = ./.;
+      npmDepsHash = "sha256-qTaCqBYDcFspNhtrm4hLDlfCg6808W9WNyFt/ejZnhY=";
     };
+  };
 }
